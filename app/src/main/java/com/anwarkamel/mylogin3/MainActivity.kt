8be11dc8358122abb469b3ac275168603cdd171b
= <=>
2020-05-26 21:52:58 -0400

    Your Commit

---
+++ app/src/main/java/com/anwarkamel/mylogin3/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.anwarkamel.mylogin3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+
+
+        startAnim()
+
+
+    }
+
+    private fun startAnim() {
+        val  ttb = AnimationUtils.loadAnimation(this , R.anim.ttb)
+        val  stb = AnimationUtils.loadAnimation(this , R.anim.stb)
+        val  btt = AnimationUtils.loadAnimation(this , R.anim.btt)
+        val  btt2 = AnimationUtils.loadAnimation(this , R.anim.btt2)
+
+
+
+
+        textAnim.startAnimation(ttb)
+
+        edt_email.startAnimation(stb)
+        edt_pass.startAnimation(stb)
+
+        btn_signup.startAnimation(btt)
+        btn_signin.startAnimation(btt)
+
+        tv_forgot_pass.startAnimation(btt2)
+
+
+    }
+
+
+}
